325642a801836bade34ee3c9fc454b44776107a2
R-Ram95 <rohinesh@ualberta.ca>
2024-03-30 18:52:59 -0600

    class for reusable socket

--- Networking/SimpleSocket.cpp
+++ Networking/SimpleSocket.cpp
@@ -1,1 +1,48 @@
 #include "SimpleSocket.hpp"
+
+// Default Constructor
+
+RAR::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface)
+{
+    // Defined address structure
+    address.sin_family = domain;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(interface);
+
+    // Establish socket
+    sock = socket(domain, service, protocol);
+    test_connection(sock);
+    // Connect socket to host device
+    connection = connect_to_network(sock, address);
+    test_connection(connection);
+}
+
+// Test connection virtual function
+
+void RAR::SimpleSocket::test_connection(int item_to_test)
+{
+    // confirm that the socket or connection has been properly established
+    if (item_to_test < 0)
+    {
+        perror("Failed to connect ...");
+        // TODO MAKE Virtual so user needs to implement what happens on failure
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Getter functions
+
+struct sockaddr_in RAR::SimpleSocket::get_address()
+{
+    return address;
+}
+
+int RAR::SimpleSocket::get_sock()
+{
+    return sock;
+}
+
+int RAR::SimpleSocket::get_connection()
+{
+    return connection;
+}
--- Networking/SimpleSocket.hpp
+++ Networking/SimpleSocket.hpp
@@ -3,13 +3,29 @@
 
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
 
-namespace rar {
-   class SimpleSocket {
-    public:
-        SimpleSocket()
+namespace RAR
+{
+   class SimpleSocket
+   {
+   private:
+      struct sockaddr_in address;
+      int sock;
+      int connection;
 
-   } 
+   public:
+      SimpleSocket(int domain, int service, int protocol, int port, u_long interface);
+      // function to connect to a network
+      virtual int connect_to_network(int sock, struct sockaddr_in address) = 0;
+      // function to test sockets and connections
+      void test_connection(int);
+      // Getter functions
+      struct sockaddr_in get_address();
+      int get_sock();
+      int get_connection();
+   };
 }
 
 #endif /* SimpleSocket_hpp */
