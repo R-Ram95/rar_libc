b03b29f13ad17da1c20e5fa756a2233e89981501
R-Ram95 <rohinesh@ualberta.ca>
2024-03-31 14:39:21 -0600

    listening socket + organization

---
+++ src/Networking/Sockets/rarlibc-sockets.hpp
@@ -0,0 +1,10 @@
+#ifndef rarlibc_sockets_hpp
+#define rarlibc_sockets_hpp
+
+#include "SimpleSocket.hpp"
+
+#include "BindingSocket.hpp"
+#include "ListeningSocket.hpp"
+
+#include "ConnectingSocket.hpp"
+#endif /* rarlibc_sockets_hpp*/
---
+++ src/Networking/rarlibc-networking.hpp
@@ -0,0 +1,5 @@
+#ifndef rarlibc_networking_hpp
+#define rarlibc_networking_hpp
+
+#include "Sockets/rarlibc-sockets.hpp"
+#endif
