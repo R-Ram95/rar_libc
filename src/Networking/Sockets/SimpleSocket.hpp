80292ee50208a9435a0862cbf5505e9c9f2a1172
R-Ram95 <rohinesh@ualberta.ca>
2024-04-01 15:14:50 -0600

    adding documentation

--- src/Networking/Sockets/SimpleSocket.hpp
+++ src/Networking/Sockets/SimpleSocket.hpp
@@ -11,11 +11,22 @@ namespace RAR
    class SimpleSocket
    {
    private:
+      // the address of the socket - see netinet/in.h for options
       struct sockaddr_in address;
+      //
       int sock;
 
    public:
       /* CONSTRUCTORS */
+      /**
+       * @brief Construct a new Simple Socket object which defines a socket and address
+       *
+       * @param domain - communication domain in which socket should be created, see sys/socket.h for more details
+       * @param service - type of socket to be created, see sys/socket.h for more detals
+       * @param protocol  - protocol to be used with the docket
+       * @param port  - transport address to be used for defining the address family when the socket is named and used in binding, connecting, or listening
+       * @param interface - the address of the socket, i.e. the ip of the hosts networking device
+       */
       SimpleSocket(int domain, int service, int protocol, int port, u_long interface);
 
       /* VIRTUAL FUNCTIONS */
