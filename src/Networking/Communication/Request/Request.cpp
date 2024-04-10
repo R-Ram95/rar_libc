587e6f226998d836366b6b249ffcb24865066e75
R-Ram95 <rohinesh@ualberta.ca>
2024-04-10 10:54:46 -0600

    working on parsing the request

--- src/Networking/Communication/Request/Request.cpp
+++ src/Networking/Communication/Request/Request.cpp
@@ -2,25 +2,33 @@
 
 RAR::Request::Request(char *request_string)
 {
-    char transformed_request[strlen(request_string)];
-    strcpy(transformed_request, request_string);
+    // make copy because we will be tokenizing
+    char request_copy[strlen(request_string) + 1];
+    strcpy(request_copy, request_string);
 
     // separate the request headers from the body - seperated by 2 blank lines (\n)
-    for (int i = 0; i < strlen(transformed_request) - 2; i++)
+    for (int i = 0; i < strlen(request_copy) - 2; i++)
     {
         // we have encountered 2 blank lines in a row, replace
-        if (transformed_request[i] == '\n' && transformed_request[i + 1] == '\n')
+        if (request_copy[i] == '\n' && request_copy[i + 1] == '\n')
         {
-            transformed_request[i + 1] = '|';
+            request_copy[i + 1] = '|';
         }
     }
 
     // split up request sections
-    char *request_line = strtok(transformed_request, "\n");
-    char *header_fields = strtok(nullptr, "|");
+    char *request_line = strtok(request_copy, "\n");
+    char *request_headers = strtok(nullptr, "|");
     char *body = strtok(nullptr, "|");
 
+    std::cout << "REQUEST LINE" << std::endl;
+    std::cout << request_line << std::endl;
+
+    std::cout << "REQUEST HEADER" << std::endl;
+    std::cout << request_headers << std::endl;
+
     parse_request_line(request_line);
+    parse_request_headers(request_headers);
 }
 
 /* PRIVATE FUNCTIONS*/
@@ -33,5 +41,19 @@ void RAR::Request::parse_request_line(char *request_line)
 
 void RAR::Request::parse_request_headers(char *request_headers)
 {
+    char header_copy[strlen(request_headers) + 1];
+    strcpy(header_copy, request_headers);
+
+    std::cout << "REQUEST HEADER in PARSE HEADERS" << std::endl;
+    std::cout << request_headers << std::endl;
+
+    std::cout << "HEADER COPY  in PARSE HEADERS" << std::endl;
+    std::cout << header_copy << std::endl;
     // go through the request header string and make a key-value mapping of headers to values (HEADER : VALUE)
+    char *header1 = strtok(header_copy, ": ");
+
+    std::cout << "header1" << std::endl;
+    std::cout << header1 << std::endl;
+    std::cout << "request headers after" << std::endl;
+    std::cout << header_copy << std::endl;
 }
